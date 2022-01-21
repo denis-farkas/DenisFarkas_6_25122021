5b160d43068120721caadc907ffc009c218e918a
denis farkas <dfarkas960@gmail.com>
2022-01-21 09:27:19 +0100

    factory item portfolio aria

--- scripts/factories/portfolioItem.js
+++ scripts/factories/portfolioItem.js
@@ -12,12 +12,16 @@ function portfolioItemFactory(data) {
       const img = document.createElement("img");
       img.setAttribute("src", `assets/media/${image}`);
       img.setAttribute("alt", `${title}`);
+      img.setAttribute("role", "link");
+      img.setAttribute("aria-label", `photographie ${title}`);
       img.setAttribute("onclick", `currentSlide(${index})`);
       article.appendChild(img);
     } else if (video) {
       const vid = document.createElement("video");
       vid.setAttribute("src", `assets/media/${video}`);
-      vid.setAttribute("autoplay",true);
+      vid.setAttribute("role", "link");
+      vid.setAttribute("aria-label", `vidéo ${title}`);
+      vid.setAttribute("autoplay",false);
       vid.setAttribute("onclick", `currentSlide(${index})`);
       article.appendChild(vid);
     }
@@ -27,6 +31,8 @@ function portfolioItemFactory(data) {
     const span = document.createElement("span");
     span.textContent = `${title}`;
     span.className="describe";
+    span.setAttribute("role", "link");
+    span.setAttribute("aria-label", `${title}`);
     span.setAttribute("onclick", `currentSlide(${index})`);
     row.appendChild(span);
     const rightSide = document.createElement("div");
@@ -35,10 +41,16 @@ function portfolioItemFactory(data) {
     const like = document.createElement("div");
     like.className="likes";
     like.textContent = `${likes}`;
+    like.setAttribute("role", "timer");
+    like.setAttribute("aria-label",`Total de j'aime ${likes}`);
     like.setAttribute("id", `${id}`);
     const i = document.createElement("i");
     i.className="fa fa-heart";
+    i.setAttribute("role", "img");
+    i.setAttribute("tabindex","0");
+    i.setAttribute("aria-label", "clicker ou enter ajoute un like");
     i.setAttribute("onclick", `liked(${id})`);
+    i.setAttribute("onkeypress",`liked(${id})`);
     rightSide.appendChild(like);
     rightSide.appendChild(i);
     return article;
